5d57552259e5dd248408a6c1315f6c547b773d8f
loelipop <loejunxiang@gmail.com>
2024-08-01 17:13:00 +0800

    新增個人資訊UI

--- app/src/main/java/fcu/app/trafficviolationdetection/PersonalInfoActivity.java
+++ app/src/main/java/fcu/app/trafficviolationdetection/PersonalInfoActivity.java
@@ -1,9 +1,6 @@
 package fcu.app.trafficviolationdetection;
 
-import android.content.Intent;
 import android.os.Bundle;
-import android.view.View;
-import android.widget.ImageButton;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -11,28 +8,17 @@ import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
-public class MainActivity extends AppCompatActivity {
-    private ImageButton PersonalInfo;
+public class PersonalInfoActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         EdgeToEdge.enable(this);
-        setContentView(R.layout.activity_main);
+        setContentView(R.layout.activity_personal_info);
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
             Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
             return insets;
         });
-        PersonalInfo = findViewById(R.id.personal_info);
-        View.OnClickListener listener = new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                Intent intent = new Intent();
-                intent.setClass(MainActivity.this, PersonalInfoActivity.class);
-                MainActivity.this.startActivity(intent);
-            }
-        };
-        PersonalInfo.setOnClickListener(listener);
     }
 }
