83502e800192119da28d8e56b15f979aa961bcdd
kenchin627 <ken0110325@gmail.com>
2024-08-14 22:21:31 +0800

    新增可檢舉違規事項頁面，檢舉紀錄頁面

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -30,6 +30,9 @@ android {
         sourceCompatibility = JavaVersion.VERSION_1_8
         targetCompatibility = JavaVersion.VERSION_1_8
     }
+    buildFeatures {
+        viewBinding = true
+    }
 }
 
 dependencies {
@@ -45,4 +48,5 @@ dependencies {
     implementation("com.google.firebase:firebase-firestore")
     implementation("com.google.firebase:firebase-auth")
     implementation("com.google.firebase:firebase-storage:21.0.0")
+    implementation("com.google.android.material:material:1.9.0")
 }
