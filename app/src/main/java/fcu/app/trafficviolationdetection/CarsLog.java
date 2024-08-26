c60c9538ce5e3c8c680db5b75587a7c3848f4fcf
kenchin627 <ken0110325@gmail.com>
2024-08-26 22:11:04 +0800

    recycle view untest

--- app/src/main/java/fcu/app/trafficviolationdetection/CarsLog.java
+++ app/src/main/java/fcu/app/trafficviolationdetection/CarsLog.java
@@ -1,14 +1,46 @@
 package fcu.app.trafficviolationdetection;
 
 import android.os.Bundle;
+import android.view.ViewGroup;
+import android.widget.Toast;
+import android.widget.TextView;
 
 import androidx.activity.EdgeToEdge;
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
+
+import com.google.android.material.bottomnavigation.BottomNavigationView;
+import com.google.android.material.navigation.NavigationBarView;
+import com.google.firebase.Firebase;
+import com.google.firebase.FirebaseApp;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+import com.google.firebase.firestore.FieldPath;
+import com.google.firebase.firestore.FirebaseFirestore;
+import com.google.firebase.firestore.QueryDocumentSnapshot;
+import com.google.firebase.storage.FirebaseStorage;
+import com.google.firebase.storage.StorageReference;
+
+import java.util.ArrayList;
+import java.util.List;
 
 public class CarsLog extends AppCompatActivity {
+
+    private FirebaseFirestore db;
+    private RecyclerView item;
+    private List<items> carsLog;
+    private RecyclerView.Adapter adapter;
+    private FirebaseAuth mAuth;
+    private FirebaseStorage storage;
+    private StorageReference storageRef;
+
+
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -20,5 +52,72 @@ public class CarsLog extends AppCompatActivity {
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
             return insets;
         });
+
+        item = findViewById(R.id.logs);
+        item.setLayoutManager(new LinearLayoutManager(this));
+
+        FirebaseApp.initializeApp(this);
+        mAuth = FirebaseAuth.getInstance();
+        db = FirebaseFirestore.getInstance();
+        storage = FirebaseStorage.getInstance();
+        storageRef = storage.getReference();
+
+        carsLog = new ArrayList<>();
+        FirebaseUser currentUser = mAuth.getCurrentUser();
+        if (currentUser != null){
+            String userId = currentUser.getUid();
+            checkUserscarLog(userId);
+        }else{
+            Toast.makeText(this, "用戶沒有登入", Toast.LENGTH_SHORT).show();
+        }
+
+
+
+        ItemAdapter adapter = new ItemAdapter(this, carsLog);
+        item.setAdapter(adapter);
+    }
+
+    private void loadCarLogFromFirestore(List<String> reportIds){
+        db.collection("report")
+                .whereIn(FieldPath.documentId(), reportIds)
+                .get()
+                .addOnCompleteListener(task -> {
+                    if (task.isSuccessful()) {
+                        for (QueryDocumentSnapshot document : task.getResult()) {
+                            String carDate = document.getString("Date");
+                            String carPlate = document.getString("ViolationsCarElph") + document.getString("ViolationsCarNum");
+                            String carRule = document.getString("Violations");
+                            String reportId = document.getString("reportId");
+
+                            carsLog.add(new items(carDate, carPlate, carRule, reportId));
+                        }
+                        item.getAdapter().notifyDataSetChanged();
+                    } else {
+                        Toast.makeText(CarsLog.this, "Error getting documents: " + task.getException(), Toast.LENGTH_LONG).show();
+                    }
+                });
+    }
+
+    private void checkUserscarLog(String userId){
+        db.collection("users_report")
+                .whereEqualTo("user_id", userId)
+                .get()
+                .addOnCompleteListener(task -> {
+                    if (task.isSuccessful()){
+                        List<String> users_reportID = new ArrayList<>();
+                        for (QueryDocumentSnapshot document : task.getResult()){
+                            users_reportID.add(document.getString("report_id"));
+                        }
+                        if (users_reportID.isEmpty()){
+                            Toast.makeText(this, "沒有違規記錄", Toast.LENGTH_SHORT).show();
+                        }else {
+                            loadCarLogFromFirestore(users_reportID);
+                        }
+                    }else{
+                        Toast.makeText(this, "發生錯誤", Toast.LENGTH_SHORT).show();
+                    }
+                });
     }
 }
+
+
--- app/src/main/java/fcu/app/trafficviolationdetection/MainActivity.java
+++ app/src/main/java/fcu/app/trafficviolationdetection/MainActivity.java
@@ -3,6 +3,7 @@ package fcu.app.trafficviolationdetection;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.Button;
 import android.widget.ImageButton;
 
 import androidx.activity.EdgeToEdge;
@@ -13,6 +14,7 @@ import androidx.core.view.WindowInsetsCompat;
 
 public class MainActivity extends AppCompatActivity {
     private ImageButton PersonalInfo;
+    private Button ViolationLog;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -25,14 +27,23 @@ public class MainActivity extends AppCompatActivity {
             return insets;
         });
         PersonalInfo = findViewById(R.id.personal_info);
+        ViolationLog = findViewById(R.id.button2);
+
         View.OnClickListener listener = new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                Intent intent = new Intent();
-                intent.setClass(MainActivity.this, PersonalInfoActivity.class);
-                MainActivity.this.startActivity(intent);
+                if (v.getId() == R.id.personal_info) {
+                    Intent intent = new Intent();
+                    intent.setClass(MainActivity.this, PersonalInfoActivity.class);
+                    MainActivity.this.startActivity(intent);
+                }else if (v.getId() == R.id.button2){
+                    Intent intent = new Intent();
+                    intent.setClass(MainActivity.this, CarsLog.class);
+                    MainActivity.this.startActivity(intent);
+                }
             }
         };
         PersonalInfo.setOnClickListener(listener);
+        ViolationLog.setOnClickListener(listener);
     }
 }
